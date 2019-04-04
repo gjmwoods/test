cf29b416a461846dd760d7a67982273892c5af55
gjmwoods <gregory.woods@axoniq.io>
2019-04-04 09:34:03 +0200

    test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ rtb.onReady(() => {
   rtb.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Sticker to shapes',
+        title: 'Generate Axon Sources',
         svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
@@ -26,10 +26,27 @@ rtb.onReady(() => {
             height: sticker.bounds.height,
           })))
 
+          // Start file download.
+          download("hello.txt","This is the content of my file :)");
+
           // Show success message
-          rtb.showNotification('Stickers has been converted')
+          rtb.showNotification('Axon Sources have been downloaded')
         }
       }
     }
   })
 })
+
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
