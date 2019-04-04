5092e8812e8d57def28b7fb753fd9ef856ff6ce2
gjmwoods <gregory.woods@axoniq.io>
2019-04-04 12:26:08 +0200

    test

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ rtb.onReady(() => {
           })))
 
           // Start file download.
-          download("hello.txt","This is the content of my file :)");
+          download("hello.txt",aggregateTemplate");
 
           // Show success message
           rtb.showNotification('Axon Sources have been downloaded')
@@ -53,4 +53,36 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
-aggregateTemplate=fs.readFileSync("./AggregateTemplate.java");
+aggregateTemplate=`package <REPLACE_WITH_PACKAGE>;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.lang.invoke.MethodHandles;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+@Aggregate
+public class <REPLACE_WITH_AGGREGATE_NAME> {
+
+    @AggregateIdentifier
+    private String id;
+
+    public <REPLACE_WITH_AGGREGATE_NAME>() {
+        log.debug("empty constructor invoked");
+    }
+
+    @CommandHandler
+    public GiftCard(IssueCmd cmd) {
+      //todo implement logic
+    }
+
+    @EventSourcingHandler
+    public void on(IssuedEvt evt) {
+
+    }
+
+}
+`
